48d339eda05ea2ee1fd1220a0e0550d152f47f74
gorm3531 <mcgermafrodit@gmail.com>
2024-05-19 00:24:12 +0700

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+
+name = input('Введите ваш текст: ')
+print(len(name)) #2
+print(name .upper()) #3
+print(name .lower())
+print(name .replace(" ", ''))
+print(name .find('H'))
+print(name .rfind("e"))
