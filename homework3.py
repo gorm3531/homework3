fdc88646937f739e73f815f71e292e2ae7b738b7
gorm3531 <mcgermafrodit@gmail.com>
2024-05-19 01:21:06 +0700

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -4,5 +4,5 @@ print(len(name)) #2
 print(name .upper()) #3
 print(name .lower())
 print(name .replace(" ", ''))
-print(name .find('H'))
-print(name .rfind("e"))
+print(name .find(''))
+print(name .rfind(""))
